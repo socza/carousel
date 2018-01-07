fde99ab1c66f7580ec7a833091158ec6923e7edd
Piotr Sociński <psocinski@gmail.com>
2018-01-07 23:51:23 +0100

    Changed names for picture files. Almost finished project.

--- js/scripts.js
+++ js/scripts.js
@@ -1,70 +1,63 @@
 $(function(){ 
 	
 	$('p').css({
-			"text-align": "center",
-			"color": "red"}
-		);
+	
+		"text-align": "center",
+		"color": "red"}
+	
+	);
 		
 	$('a').hover(function(){
+		
 			$(this).css("transform", "scale(1.50)");
+	   	
 	   	}, function(){
+	   	
 	   		$(this).css("transform", "scale(1)");
-		}
-	);
 	
-	// iterowanie
-	$('li').each(function(index, element){
-		console.log(index);
+	});
+	
+  	$('.left-cursor').on("click", function(event) {
+			
+		// console.log("You clicked the left-coursor!");
+		moveLeft();
+		
 	});
 
-	// ======== START FOR LEFT CAROUSEL ======== //
-
-		// var carouselList = $("#carousel ul");
-	  	
-	 //    function moveFirstSlide() {
-	 //  		var firstItem = carouselList.find("li:first"); 
-	 //    	var lastItem = carouselList.find("li:last");
-	    
-	 //    	lastItem.after(firstItem);
-	 //    	carouselList.css({marginLeft: 0});
-	 //  	}
-
-	 //  	function changeSlide() {
-	 //    	carouselList.animate({'marginLeft': -400}, 500, moveFirstSlide);
-	 //  	}
-
-	 //  	setInterval(changeSlide, 3000);
-
-	 //  	$('.previous').on("click", function(event) {
-		// 	console.log("You clicked theb previous!");
-		// 	changeSlide();
-		// });
-
-  	// ========= END LEFT CAROUSEL ======== //
-
-  	// ========= START FOR RIGHT CAROUSEL ======== //
-
-		var carouselList = $("#carousel ul");
-
-  		function moveFirstSlide() {
-	  		var firstItem = carouselList.find("li:first"); 
-	    	var lastItem = carouselList.find("li:last");
-	    
-	    	firstItem.after(lastItem);
-	    	carouselList.css({marginLeft: 0});
-	  	}
-
-	  	function changeSlideRight() {
-	    	carouselList.animate({'marginLeft': 400}, 500, moveFirstSlide);
-	  	}
+  	$('.right-cursor').on("click", function(event) {
 		
-	  	setInterval(changeSlideRight, 3000);
+		// console.log("You clicked the right-coursor!");
+		moveRight();
+	
+	});
 
-		$('.next').on("click", function(event) {
-			console.log("You clicked the next!");
-			changeSlideRight();
-		});
+	var carouselList = $("#carousel ul");
+	  	
+	function moveLeft() {
+	
+		var firstItem = carouselList.find("li:first"); 
+	   	var lastItem = carouselList.find("li:last");
+	    
+	   	lastItem.after(firstItem);
+	   	carouselList.css({marginLeft: 0});
+	}
 
-	// ========= END FOR RIGHT CAROUSEL ======== //	
+	function changeSlide() {
+	
+	   	carouselList.animate({'marginLeft': -400}, 0, moveLeft);
+	
+	}
+
+  	function moveRight() {
+	    	
+		var firstItem = carouselList.find("li:first"); 
+	   	var lastItem = carouselList.find("li:last");
+	    	
+	   	firstItem.before(lastItem);
+		carouselList.animate({'marginLeft': 0}, 0);
+
+	}
+	
+	setInterval(changeSlide, 3000);  	
 
 });
