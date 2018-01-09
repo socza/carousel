0cfb5bbab78d14bfb3ae2927b65632d1437292b5
Piotr Sociński <psocinski@gmail.com>
2018-01-09 22:01:07 +0100

    Finished function for left and right cursor for images animate.

--- js/scripts.js
+++ js/scripts.js
@@ -20,32 +20,40 @@ $(function(){
   	$('.left-cursor').on("click", function(event) {
 			
 		// console.log("You clicked the left-coursor!");
-		moveLeft();
+		changeSlideLeft();
 		
 	});
 
   	$('.right-cursor').on("click", function(event) {
 		
 		// console.log("You clicked the right-coursor!");
-		moveRight();
+		changeSlideRight();
 	
 	});
 
 	var carouselList = $("#carousel ul");
 	  	
+
+	function changeSlideLeft() {
+	
+	   	carouselList.animate({'marginLeft': -400}, 500, moveLeft);
+	
+	}
+
+	function changeSlideRight() {
+	
+	   	carouselList.css({'marginLeft': -400});
+	   	moveRight();
+		
+	}
+
 	function moveLeft() {
 	
 		var firstItem = carouselList.find("li:first"); 
 	   	var lastItem = carouselList.find("li:last");
 	    
 	   	lastItem.after(firstItem);
-	   	carouselList.css({marginLeft: 0});
-	}
-
-	function changeSlide() {
-	
-	   	carouselList.animate({'marginLeft': -400}, 500, moveLeft);
-	
+	   	carouselList.css({'marginLeft': 0});
 	}
 
   	function moveRight() {
@@ -54,10 +62,10 @@ $(function(){
 	   	var lastItem = carouselList.find("li:last");
 	    	
 	   	firstItem.before(lastItem);
-		carouselList.animate({'marginLeft': 0});
-
+		carouselList.animate({'marginLeft': 0}, 500);
+		
 	}
 	
-	setInterval(changeSlide, 3000);  	
+	setInterval(changeSlideLeft, 3000);  	
 
 });
